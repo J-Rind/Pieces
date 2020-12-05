abf60755ce8a696a80791a92672d9744b98d0b35
Aiden717 <71095738+Aiden717@users.noreply.github.com>
2020-12-05 13:29:03 -0800

    Rearranging code

--- code/src/com/company/Piece.java
+++ code/src/com/company/Piece.java
@@ -1,4 +1,5 @@
 package com.company;
+import java.util.ArrayList;
 
 import java.security.KeyStore;
 
@@ -9,7 +10,7 @@ public class Piece {
     private String type;
     private Boolean isAlive;
     private Boolean isWhite;
-
+    public ArrayList<int[]> range = new ArrayList<>();
 
     Piece(int x, int y, String t, Boolean white)
     {
@@ -18,18 +19,43 @@ public class Piece {
         type = t;
         isAlive = Boolean.TRUE;
         isWhite = white;
+        range = new ArrayList<>();
     }
 
     Piece()
     {}
 
-    public void moveTo(int x, int y){} //function should change piece coordinates after error checking
+    /*public void moveTo(int x, int y, King myKing, ArrayList<Piece> arr){
+        int[] myCoords = new int[]{this.getX(), this.getY()};
+        int moved = 0;
+        for (int[] coordinate : this.range) {
+            if (coordinate[0] == myCoords[0] && coordinate[1] == myCoords[1])
+                {
+                    this.setX(x);
+                    this.setY(y);
+                    System.out.println("Piece Moved to: (" + x + "," + y + ")");
+                    moved++;
+                    break;
+                }
+        if(moved != 1)
+        {
+            System.out.println("piece not moved, not in range");
+        }
+            if(myKing.kingCheck(arr) == false) //this is the fucntion for king check, this needs quite a bit more work.
+            {
+            this.setX(x);
+            this.setY(y);
+            System.out.println("Piece Moved to: (" + x + "," + y + ")");
+            }else{
+                System.out.println("invalid move, king is in check");
+                }
+    } //function should change piece coordinates after error checking*/
 
-    private boolean validMove(int x, int y){return true;} //takes destination coordinates and returns a boolean indicating if a move is valid or not
 
-    private int getRange(){return 1;} //return type tbd, function should return the current range of movement for each piece, assuming empty board
 
-    private boolean kingCheck(){return true;} //returns if this move will place the current player's king in check.
+    public void getRange (){} //return type tbd, function should return the current range of movement for each piece, assuming empty board
+
+    public boolean kingCheck(){return true;} //returns if this move will place the current player's king in check.
 
     public void print(){
         System.out.println("Coordinate: (" + X + "," + Y + ")");
@@ -38,6 +64,22 @@ public class Piece {
         System.out.println("Status: " + isAlive);
 
 
+
+    }
+
+    public int getPiece(int dX, int dY,ArrayList<Piece> arr)
+    {
+        for (int i =0; i < arr.size(); i++){
+            if (arr.get(i).getX() == dX && arr.get(i).getY() == dY )
+            {
+                if (arr.get(i).getColor() == this.isWhite)
+                {return 1;}
+                else {return 2;}
+            }
+
+
+        }
+        return 0;
 
     }
 
@@ -52,5 +94,4 @@ public class Piece {
     public void setType(String tipe){type = type;}
     public void setIsAlive(Boolean s){isAlive = s;}
     public void setIsWhite(Boolean a){isWhite = a;}
-
 }
