2348b8466c17ccc06a312b326a3bcf355e4a7709
Aiden717 <71095738+Aiden717@users.noreply.github.com>
2020-12-05 18:22:49 -0800

    getRange

--- code/src/com/company/King.java
+++ code/src/com/company/King.java
@@ -3,22 +3,65 @@ package com.company;
 import java.util.ArrayList;
 
 class King extends Piece {
-    public King(int[] coordinates) {
-        super(coordinates);
+    public King(int x, int y, String t, Boolean white) {
+        super(x, y, t, white);
     }
 
     // Params:
-    // Board board: A copy of the current board; used to find out where pieces are
+    // ArrayList<Piece> pieces: An ArrayList of all the pieces on the board
+    // Desc:
+    // Checks the valid move range for a piece on an open board, checks to see if pieces are in that square,
+    // if they are not or are an enemy, the range is updated to add the new piece.
+    @Override
+    public void getRange(ArrayList<Piece> pieces){
+        this.range.clear();
+        int  pieceX = this.getX(); int pieceY = this.getY(); // X and Y coordinates for the king piece
+
+        //Up
+        if (this.getPiece(pieceX, pieceY+1, pieces) == 0 || this.getPiece(pieceX, pieceY+1, pieces) == 2)
+            this.range.add(new int[] {pieceX, pieceY+1});
+
+        //Up-right
+        if (this.getPiece(pieceX+1, pieceY+1, pieces) == 0 || this.getPiece(pieceX+1, pieceY+1, pieces) == 2)
+            this.range.add(new int[] {pieceX+1, pieceY+1});
+
+        //Right
+        if (this.getPiece(pieceX+1, pieceY, pieces) == 0 || this.getPiece(pieceX+1, pieceY, pieces) == 2)
+            this.range.add(new int[] {pieceX+1, pieceY});
+
+        //Down-right
+        if (this.getPiece(pieceX+1, pieceY-1, pieces) == 0 || this.getPiece(pieceX+1, pieceY-1, pieces) == 2)
+            this.range.add(new int[] {pieceX+1, pieceY-1});
+
+        //Down
+        if (this.getPiece(pieceX, pieceY-1, pieces) == 0 || this.getPiece(pieceX, pieceY-1, pieces) == 2)
+            this.range.add(new int[] {pieceX, pieceY-1});
+
+        //Down-left
+        if (this.getPiece(pieceX-1, pieceY-1, pieces) == 0 || this.getPiece(pieceX-1, pieceY-1, pieces) == 2)
+            this.range.add(new int[] {pieceX-1, pieceY-1});
+
+        //Left
+        if (this.getPiece(pieceX-1, pieceY, pieces) == 0 || this.getPiece(pieceX-1, pieceY, pieces) == 2)
+            this.range.add(new int[] {pieceX-1, pieceY});
+
+        //Up-left
+        if (this.getPiece(pieceX-1, pieceY+1, pieces) == 0 || this.getPiece(pieceX-1, pieceY+1, pieces) == 2)
+            this.range.add(new int[] {pieceX-1, pieceY+1});
+    }
+
+    // Params:
+    // ArrayList<Piece> pieces: An ArrayList of all the pieces on the board
     // Returns:
     // boolean check: true = in-check; false = out-of-check
-    boolean kingCheck(Board board) {
+    public boolean kingCheck(ArrayList<Piece> pieces) {
         boolean check = false;
-        ArrayList<Piece> surrPieces = this.checkSurroundings(board); //Get pieces surrounding board
+        int[] kingCoords = new int[]{this.getX(), this.getY()};
 
-        if (surrPieces.size() > 0) {
-            for (Piece surrPiece : surrPieces) { //Foreach piece in surrPieces
-                if (surrPiece.isWhite != this.isWhite) { //If piece is opposite to king
-                    if (surrPiece.getRange().contains(this.coordinates)) { //Is king in range of piece
+        for (Piece piece : pieces) {
+            if (piece.getColor() != this.getColor()) { // If the color does not match the king's color...
+                for (int[] coordinate : piece.range) {
+                    if (coordinate[0] == kingCoords[0] && coordinate[1] == kingCoords[1]) { // Checks to see if the king is in range of enemy pieces
                         check = true;
                         break;
                     }
@@ -26,109 +69,5 @@ class King extends Piece {
             }
         }
         return check;
-    }
-
-    // Params:
-    // Board board: A copy of the current board; used to find out where pieces are
-    // Returns:
-    // ArrayList<Piece> pieces: An ArrayList of the pieces surrounding the king
-    ArrayList<Piece> checkSurroundings(Board board) {
-        ArrayList<Piece> pieces = new ArrayList<>();
-        int[] kingCoords = this.coordinates;
-
-        //Check rays for pieces
-        //Above king
-        for (int x = kingCoords[1], y = kingCoords[2] + 1; y < 8; y++) {
-            if (board.squares[x][y].getPiece() != null) {
-                pieces.add(board.squares[x][y].getPiece());
-                break;
-            }
-        }
-
-        //Below king
-        for (int x = kingCoords[1], y = kingCoords[2] - 1; y > -1; y--) {
-            if (board.squares[x][y].getPiece() != null) {
-                pieces.add(board.squares[x][y].getPiece());
-                break;
-            }
-        }
-        //Right of king
-        for (int x = kingCoords[1] + 1, y = kingCoords[2]; x < 8; x++) {
-            if (board.squares[x][y].getPiece() != null) {
-                pieces.add(board.squares[x][y].getPiece());
-                break;
-            }
-        }
-        //Left of king
-        for (int x = kingCoords[1] - 1, y = kingCoords[2]; x > -1; x--) {
-            if (board.squares[x][y].getPiece() != null) {
-                pieces.add(board.squares[x][y].getPiece());
-                break;
-            }
-        }
-        //Up-Right
-        for (int x = kingCoords[1] + 1, y = kingCoords[2] + 1; x < 8 && y < 8; x++, y++) {
-            if (board.squares[x][y].getPiece() != null) {
-                pieces.add(board.squares[x][y].getPiece());
-                break;
-            }
-        }
-        //Down-Right
-        for (int x = kingCoords[1] + 1, y = kingCoords[2] - 1; x < 8 && y > -1; x++, y--) {
-            if (board.squares[x][y].getPiece() != null) {
-                pieces.add(board.squares[x][y].getPiece());
-                break;
-            }
-        }
-        //Down-Left
-        for (int x = kingCoords[1] - 1, y = kingCoords[2] - 1; x > -1 && y > -1; x--, y--) {
-            if (board.squares[x][y].getPiece() != null) {
-                pieces.add(board.squares[x][y].getPiece());
-                break;
-            }
-        }
-        //Up-Left
-        for (int x = kingCoords[1] - 1, y = kingCoords[2] + 1; x > -1 && y < 8; x--, y++) {
-            if (board.squares[x][y].getPiece() != null) {
-                pieces.add(board.squares[x][y].getPiece());
-                break;
-            }
-        }
-
-        //Get potential attacking knights
-        //Right 1, Up 2
-        if (kingCoords[1] + 1 < 8 && kingCoords[2] + 2 < 8)
-            if (board.squares[kingCoords[1] + 1][kingCoords[2] + 2].getPiece() != null)
-                pieces.add(board.squares[kingCoords[1] + 1][kingCoords[2] + 2].getPiece());
-        //Right 2, Up 1
-        if (kingCoords[1] + 2 < 8 && kingCoords[2] + 1 < 8)
-            if (board.squares[kingCoords[1] + 2][kingCoords[2] + 1].getPiece() != null)
-                pieces.add(board.squares[kingCoords[1] + 2][kingCoords[2] + 1].getPiece());
-        //Right 2, Down 1
-        if (kingCoords[1] + 2 < 8 && kingCoords[2] - 1 > -1)
-            if (board.squares[kingCoords[1] + 1][kingCoords[2] - 1].getPiece() != null)
-                pieces.add(board.squares[kingCoords[1] + 1][kingCoords[2] - 1].getPiece());
-        //Right 1, Down 2
-        if (kingCoords[1] + 1 < 8 && kingCoords[2] - 2 > -1)
-            if (board.squares[kingCoords[1] + 1][kingCoords[2] - 2].getPiece() != null)
-                pieces.add(board.squares[kingCoords[1] + 1][kingCoords[2] - 2].getPiece());
-        //Left 1, Down 2
-        if (kingCoords[1] - 1 > -1 && kingCoords[2] - 2 > -1)
-            if (board.squares[kingCoords[1] - 1][kingCoords[2] - 2].getPiece() != null)
-                pieces.add(board.squares[kingCoords[1] - 1][kingCoords[2] - 2].getPiece());
-        //Left 2, Down 1
-        if (kingCoords[1] - 2 > -1 && kingCoords[2] - 1 > -1)
-            if (board.squares[kingCoords[1] - 2][kingCoords[2] - 1].getPiece() != null)
-                pieces.add(board.squares[kingCoords[1] - 2][kingCoords[2] - 1].getPiece());
-        //Left 2, Up 1
-        if (kingCoords[1] - 2 < 8 && kingCoords[2] + 1 < 8)
-            if (board.squares[kingCoords[1] - 2][kingCoords[2] + 1].getPiece() != null)
-                pieces.add(board.squares[kingCoords[1] - 2][kingCoords[2] + 1].getPiece());
-        //Left 1, Up 2
-        if (kingCoords[1] - 1 < 8 && kingCoords[2] + 2 < 8)
-            if (board.squares[kingCoords[1] - 1][kingCoords[2] + 2].getPiece() != null)
-                pieces.add(board.squares[kingCoords[1] - 1][kingCoords[2] + 2].getPiece());
-
-        return pieces;
     }
 }
